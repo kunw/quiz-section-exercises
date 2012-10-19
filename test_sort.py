3c536a8df14e477b5984fb2eb4ed4bd1b94cd6cf
Erik Chou <ekchou@ymail.com>
2012-10-19 13:57:52 -0700

    Added unit tests skeleton

---
+++ test_sort.py
@@ -0,0 +1,24 @@
+import unittest
+
+class TestSortFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.list1 = [
+            'https://www.google.com',
+            'https://github.com/'
+        ]
+
+    def test_sort1(self):
+        self.assertTrue(1 == 1)
+        
+    def test_sort2(self):
+        self.assertTrue(1 == 1)
+
+    def test_sort3(self):
+        self.assertTrue(1 == 1)
+        
+    def test_sort4(self):
+        self.assertTrue(1 == 1)        
+
+if __name__ == '__main__':
+    unittest.main()
