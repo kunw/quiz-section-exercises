ef7c8d3fa55d911784d35acb18b9a1fae1f57da3
Erik <ekchou@ymail.com>
2012-10-24 12:43:08 -0700

    Modified 'main.py'
    - Added function that takes in file name and sorting function
    - Automatically runs sorting function with default arguments if the file is run

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+def sortUrls(filename, sort_func):
+  # Read in list of urls from file
+  url_list = open(filename).readlines()
+  
+  # Sort urls
+  #url_list = sort_func(url_list)
+  sort_func(url_list)
 
-# Read in URLs from file
-urls = open('urls.txt').readlines()
+  # Print out sorted URLs to file
+  f =  open('sorted_urls.txt', 'w')
+  for url in url_list:
+    f.write(url)
 
-# Sort URLs
-# TODO: need to have sorting functions implemented first
+def sortList(urls):
+  # do nothing
+  pass
 
-# Print out sorted URLs to file
-f =  open('sorted_urls.txt', 'w')
-for url in urls:
-  f.write(url)
-
+if __name__ == '__main__':
+  sortUrls('urls.txt', sortList)
