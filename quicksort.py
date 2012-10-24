fe53b5d7f977a2df49ce866f34a6f89af0b1d363
maeday <yukiorm@gmail.com>
2012-10-24 16:31:55 -0700

    Tested quicksort and changed code so that it actually works.

--- quicksort.py
+++ quicksort.py
@@ -3,22 +3,22 @@
 # Simple version defined in wikipedia (uses O(n) memory)
 def quicksort(elements):
     # If one or zero elements, then just return (technically already sorted)
-    if (elements.getNumElements() <= 1):
+    if (len(elements) <= 1):
         return elements
     else:
         # Choose pivot and then create lists containing elements that are
         # less than and greater than the pivot element.
-        pivot = elements.getNumElements() / 2
+        pivot = elements[len(elements) / 2]
         left = []
         right = []
-        for element in elements:
-            if element <= pivot:
-                left.append(len(left), element)
-            else:
-                right.append(len(right), element)
+
+        for i in range(len(elements)):
+            if (i != len(elements)/2):
+                if (elements[i] <= pivot):
+                    left.append(elements[i])
+                else:
+                    right.append(elements[i])
 
         # Now just recursively call quicksort on the left & right lists and combine
         # at the end with the pivot element.
         return quicksort(left) + [pivot] + quicksort(right)
-
-
