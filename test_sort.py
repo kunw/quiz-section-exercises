52a104070883d610a3b1bb5f322b43e2491c0bd7
Kun <kunw@cs.washington.edu>
2012-10-24 22:06:02 -0700

    add ascii tests

--- test_sort.py
+++ test_sort.py
@@ -5,9 +5,21 @@ from sortalgs import mergesort, quicksort, radixsort
 class TestSortFunctions(unittest.TestCase):
 
     def setUp(self):
-        self.list1 = [
+        self.list_url = [
             'https://www.google.com',
-            'https://github.com/'
+            'https://github.com/',
+            'http://www.google.com',
+            'http://www.facebook.com',
+            'http://www.facebook.com/groups/405662882833858/407845452615601/?comment_id=407863142613832&notif_t=group_comment_reply',
+            'http://www.stackoverflow.com/'
+        ]
+        self.list_url_sorted = [
+            'http://www.facebook.com',
+            'http://www.facebook.com/groups/405662882833858/407845452615601/?comment_id=407863142613832&notif_t=group_comment_reply',
+            'http://www.google.com',
+            'http://www.stackoverflow.com/',
+            'https://github.com/',
+            'https://www.google.com'
         ]
         self.list_char = [
             'b', 'c', 'a', 'd', 'e', 'f', 'z', 'q'
@@ -22,26 +34,30 @@ class TestSortFunctions(unittest.TestCase):
         self.list_string_sorted = [
         		'12zzt2kz777', '12zztt5j', '62lll', 'a9bc2', 'abc2', 'abc3', 'acjk', 'badbz7l', 'bade', 'bade5j', 'qqqqqj',  'z1jlk'
         ]
+        
+        self.list_ascii = [
+            'r', 'R', '/', '.', ';', '+', '>', '<', '=', '?', '-', '%', '[', '5'
+        ]
+        self.list_ascii_sorted = [
+        		'%', '+', '-', '.', '/', '5', ';', '<', '=', '>', '?', 'R', '[', 'r',
+        ]
 
     def test_char_quicksort(self):
         self.assertTrue(self.list_char_sorted == quicksort(self.list_char))
         
     def test_char_mergesort(self):
-        print(mergesort(self.list_char))
         self.assertTrue(self.list_char_sorted == mergesort(self.list_char))
         
     def test_char_radixsort(self):
         self.assertTrue(self.list_char_sorted == radixsort(self.list_char))
         
     def test_string_quicksort(self):
-        print(quicksort(self.list_string))
         self.assertTrue(self.list_string_sorted == quicksort(self.list_string))
         
     def test_string_mergesort(self):
         self.assertTrue(self.list_string_sorted == mergesort(self.list_string))
         
     def test_string_radixsort(self):
-        print(radixsort(self.list_string))
         self.assertTrue(self.list_string_sorted == radixsort(self.list_string))
         
     def test_empty_quicksort(self):
@@ -61,6 +77,24 @@ class TestSortFunctions(unittest.TestCase):
         
     def test_one_radixsort(self):
         self.assertTrue(['a'] == radixsort(['a']))
+    
+    def test_ascii_quicksort(self):
+        self.assertTrue(self.list_ascii_sorted == quicksort(self.list_ascii))
+        
+    def test_ascii_mergesort(self):
+        self.assertTrue(self.list_ascii_sorted == mergesort(self.list_ascii))
+        
+    def test_ascii_radixsort(self):
+        self.assertTrue(self.list_ascii_sorted == radixsort(self.list_ascii))
+        
+    def test_url_quicksort(self):
+        self.assertTrue(self.list_url_sorted == quicksort(self.list_url))
+        
+    def test_url_mergesort(self):
+        self.assertTrue(self.list_url_sorted == mergesort(self.list_url))
+        
+    def test_url_radixsort(self):
+        self.assertTrue(self.list_url_sorted == radixsort(self.list_url))
         
     def test_char_quicksort(self):
         self.assertTrue(1 == 1)
