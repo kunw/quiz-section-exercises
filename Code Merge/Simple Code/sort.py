6f845317dbd982adf38f2856cfd4ae035677d8dc
maeday <yukiorm@gmail.com>
2012-11-14 23:44:45 -0800

    Changed simple code so that it now has flags.

--- Code Merge/Simple Code/sort.py
+++ Code Merge/Simple Code/sort.py
@@ -1,5 +1,4 @@
-import sys
-import sys
+import sys, getopt
 from sortalgs import mergesort, quicksort, radixsort, selectionsort, insertionsort, bucketsort, mergesort2, quicksort2
 
 def sortfile(filename, sort_func):
@@ -19,7 +18,66 @@ def sortfile(filename, sort_func):
     for url in url_list:
         f.write(url)
 
-def main(arg):
+# Skeleton function for now...
+def candv(filename):
+    return True
+
+def main(args):
+    filename = ''
+    func = ''
+    sortfunc = None
+    validate = False
+
+    try:
+        opts, args = getopt.getopt(args,"hi:vf:",["ifile=", "valid", "func="])
+    except getopt.GetoptError:
+        print 'Invalid number of arguments!'
+        print 'Usage: sort.py -i [input file] -v -f [sortfunc]'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'Usage: sort.py -i [input file] -v -f [sortfunc]'
+            sys.exit()
+        elif opt in ("-i", "ifile"):
+            print arg
+            filename = arg
+        elif opt in ("-f", "--func"):
+            print arg
+            func = arg
+        elif opt in ("-v", "--valid"):
+            validate = True
+
+    if filename == '' or func == '':
+        print 'Invalid filename or sorting function!'
+        print 'Usage: sort.py -i [input file] -v -f [sortfunc]'
+        sys.exit(2)
+    else:
+        if func == 'mergesort':
+            sortfunc = mergesort
+        elif func == 'quicksort':
+            sortfunc = quicksort
+        elif func == 'radixsort':
+            sortfunc = radixsort
+        elif func == 'selectionsort':
+            sortfunc = selectionsort
+        elif func == 'insertionsort':
+            sortfunc = insertionsort
+        elif func == 'bucketsort':
+            sortfunc = bucketsort
+        elif func == 'mergesort2':
+            sortfunc = mergesort2
+        elif func == 'quicksort2':
+            sortfunc = quicksort2
+        else:
+            raise Exception('Invalid sort function: ' + func)
+
+        if validate:
+            valid = candv(filename)
+
+            if valid:
+                sortfile(filename, sortfunc)                
+
+"""
     if len(arg) != 2:
         print 'Invalid number of arguments!'
         print 'Usage: python sort.py [filename] [sortfunc]'
@@ -46,6 +104,7 @@ def main(arg):
         else:
             raise Exception('Invalid sort function: ' + func)
         sortfile(filename, sortfunc)
+"""
 
 if __name__ == "__main__":
     main(sys.argv[1:])
